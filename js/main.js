27c0bb69d6496ffdba28538a099650b14a4442e8
LunaPixu <officialtrxd@gmail.com>
2023-02-03 17:37:54 +0000

    Moving proj to Harp

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-let trimMaterialBox = document.getElementById("trim-material");
-let modelIndexBox = document.getElementById("model-index");
-let rgbColor = document.getElementById("rgb-tooltip");
+let trimValues = {
+  material : document.getElementById("trim-material").value,
+  modelIndex: document.getElementById("model-index").value,
+  tooltipColor: document.getElementById("rgb-tooltip").value
+};
